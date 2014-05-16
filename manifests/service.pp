09efd4da892ff0ec2c97888d62ce5ec5e858fc48
Ian Unruh <ianunruh@gmail.com>
2014-05-16 11:38:46 -0500

    Fixed service state management

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,62 +1,59 @@
 class skyline::service inherits skyline {
 
-  if $enable_analyzer {
-    file { '/etc/init/skyline-analyzer.conf':
-      ensure => present,
-      content => template('skyline/skyline-analyzer.conf.erb')
-    }
+  file { '/etc/init/skyline-analyzer.conf':
+    ensure => present,
+    content => template('skyline/skyline-analyzer.conf.erb')
+  }
 
+  file { '/etc/init/skyline-horizon.conf':
+    ensure => present,
+    content => template('skyline/skyline-horizon.conf.erb')
+  }
+
+  file { '/etc/init/skyline-webapp.conf':
+    ensure => present,
+    content => template('skyline/skyline-webapp.conf.erb')
+  }
+
+  if $enable_analyzer {
     service { 'skyline-analyzer':
       ensure => running,
+      enable => true,
       require => File['/etc/init/skyline-analyzer.conf']
     }
   } else {
-    file { '/etc/init/skyline-analyzer.conf':
-      ensure => absent
-    }
-
     service { 'skyline-analyzer':
-      ensure => stopped
+      ensure => stopped,
+      enable => false,
+      require => File['/etc/init/skyline-analyzer.conf']
     }
   }
 
   if $enable_horizon {
-    file { '/etc/init/skyline-horizon.conf':
-      ensure => present,
-      content => template('skyline/skyline-horizon.conf.erb')
-    }
-
     service { 'skyline-horizon':
       ensure => running,
+      enable => true,
       require => File['/etc/init/skyline-horizon.conf']
     }
   } else {
-    file { '/etc/init/skyline-horizon.conf':
-      ensure => absent
-    }
-
     service { 'skyline-horizon':
-      ensure => stopped
+      ensure => stopped,
+      enable => false,
+      require => File['/etc/init/skyline-horizon.conf']
     }
   }
 
   if $enable_webapp {
-    file { '/etc/init/skyline-webapp.conf':
-      ensure => present,
-      content => template('skyline/skyline-webapp.conf.erb')
-    }
-
     service { 'skyline-webapp':
       ensure => running,
+      enable => true,
       require => File['/etc/init/skyline-webapp.conf']
     }
   } else {
-    file { '/etc/init/skyline-webapp.conf':
-      ensure => absent
-    }
-
     service { 'skyline-webapp':
-      ensure => stopped
+      ensure => stopped,
+      enable => false,
+      require => File['/etc/init/skyline-webapp.conf']
     }
   }
 
